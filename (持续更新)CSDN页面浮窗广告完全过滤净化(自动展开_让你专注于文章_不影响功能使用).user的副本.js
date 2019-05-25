c1e29802148e37d29d18de8428d4c858541d9b0d
Adlered <1101635162@qq.com>
2019-05-25 20:25:28 +0800

    Update (持续更新)CSDN页面浮窗广告完全过滤净化(自动展开_让你专注于文章_不影响功能使用).user的副本.js

--- (持续更新)CSDN页面浮窗广告完全过滤净化(自动展开_让你专注于文章_不影响功能使用).user的副本.js
+++ (持续更新)CSDN页面浮窗广告完全过滤净化(自动展开_让你专注于文章_不影响功能使用).user的副本.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         (持续更新)CSDN页面浮窗广告完全过滤净化(净化复制内容|自动展开|让你专注于文章|不影响功能使用)
 // @namespace    https://github.com/AdlerED
-// @version      1.0.8
-var version = "1.0.8";
+// @version      1.0.9
+var version = "1.0.9";
 // @description  轻量级TamperMonkey插件：CSDN页面浮窗广告完全过滤净化 By Adler
 // @author       Adler
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
+// @note         19-03-01 1.0.9 感谢油叉用户“渣渣不准说话”的反馈，修复了收藏按钮消失的问题
 // @note         19-03-01 1.0.3 添加页面选择性过滤规则
 // @note         19-03-01 1.0.2 增加了净化剪贴板功能
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
@@ -105,8 +106,8 @@ function killAll() {
         "blog_tip_box",
         //推荐关注用户
         "blog-expert-recommend-box",
-        //右侧工具栏
-        "tool-box",
+        //右侧工具栏（注释原因：收藏消失）
+        //"tool-box",
         //右下角VIP
         "meau-gotop-box",
         //广告
