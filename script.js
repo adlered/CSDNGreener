826dbc02b8dcbba3121bdca9b1668f0e535a0165
AdlerED <AdlerED@outlook.com>
2019-10-30 14:10:47 +0800

    2.0.0 祝自己生日快乐~完全重写CSDNGreener，统一使用JQuery，效率更高

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         (持续更新)CSDN页面浮窗广告完全过滤净化(净化复制内容|自动展开|让你专注于文章|不影响功能使用)
 // @namespace    https://github.com/AdlerED
-// @version      1.5.2
-var version = "1.5.2";
-// @description  拥有数项独家功能的最强脚本，不信就对比试试！|CSDN|博客|✔️独家原创文章免登录展开|✔️独家推荐内容自由开关|✔️独家超级免会员|✔️独家免登录复制|✔️独家防外链重定向|✔️独家论坛未登录自动展开文章、评论|✔️全面净化|✔️沉浸阅读|✔️净化剪贴板
+// @version      2.0.0
+var version = "2.0.0";
+// @description  ❤拥有数项独家功能的最强脚本，不信比一比|✔️超级预处理|✔️独家原创文章免登录展开|✔️独家推荐内容自由开关|✔️独家超级免会员|✔️独家免登录复制|✔️独家防外链重定向|✔️独家论坛未登录自动展开文章、评论|✔️全面净化|✔️沉浸阅读|✔️净化剪贴板
 // @author       Adler
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js
+// @note         19-10-30 2.0.0 祝自己生日快乐~完全重写CSDNGreener，统一使用JQuery，效率更高
 // @note         19-10-27 1.5.2 删除分享海报提示&&感谢GitHub的朋友“CHN-STUDENT”的反馈，去除底部课程推荐
 // @note         19-10-27 1.5.1 感谢来自GitHub的朋友“CHN-STUDENT”的细致复现反馈，去除了底部的课程推荐广告
 // @note         19-10-04 1.5.0 移除了底部主题信息和打赏
@@ -39,248 +40,304 @@ var version = "1.5.2";
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
+var currentURL = window.location.href;
+var list;
 
 (function () {
-    var currentURL = window.location.href;
-    console.log("你正在访问 " + currentURL + " , 正在为你匹配过滤规则......");
+    'use strict';
+
+    l("CSDNGreener V" + version);
+    var blockURL = currentURL.split("/").length;
+    var main = /(www\.csdn\.net\/)$/;
     var article = /article/;
-    var postedit = /postedit/;
-    console.log("欢迎, 正在执行CSDN净化插件! Powered By Adler WeChat: 1101635162");
-    var count = 0;
-    if (count == 0) {
-        console.log("正在进行第一次Kill操作......");
-        killAll();
-    }
+    var bbs = /bbs\.csdn\.net/;
+    var blog = /blog\.csdn\.net/;
+    var blog2 = /\/article\/list\//;
+    var download = /download\.csdn\.net/;
 
-    //CSDN臭不要脸重定向外链
-    $("#content_views").off();
-    //去除推广广告
-    $("li[data-type='ad']").remove();
+    // 数组初始化
+    list = [];
+    // 头部分
+    // APP
+    put(".app-app");
+    // VIP
+    put(".vip-caise");
+    // Cookie
+    common(6, 1);
 
-    //赚零钱改为GitChat(仅运行一次)
-    //var gitChat = document.getElementsByClassName("gitChat");
-    //gitChat[0].remove();
-    //主页右侧的今日推荐
-    var rightBox = document.getElementsByClassName("right_box");
-    //rightBox[0].remove();
-    //setTimeout(function (){
-    //    rightBox[0].remove();
-    //    rightBox[1].remove();
-    //}, 1000)
-    //干掉右侧栏，并让左侧栏填充屏幕
-    if (article.test(currentURL)) {
-        $("aside").remove();
-        $("main").css("width", "100%");
-        //去除评论区上面的广告
-        $("div#dmp_ad_58").remove();
-    }
-    //右侧栏靠右
-    $(".tool-box").css("right", "0px");
-
-    //去除剪贴板劫持
-    try {
-        csdn.copyright.init("", "", "");
-    } catch (err) {}
-
-    //免登录复制
-    $(".hljs-button").attr("data-title", "免登录复制");
-    $(".hljs-button").attr("onclick", "hljs.copyCode(event)");
-
-    //推荐内容开关cookie
-    var removeCookie = $.cookie("remove");
-    var remove;
-    if (removeCookie == undefined) {
-        $.cookie('remove', true, {
-            path: '/'
-        });
-        remove = true;
-    }
-    if (removeCookie == "true") {
-        remove = true;
+    if (main.test(currentURL)) {
+        l("正在优化主页体验...");
+        // 常规
+        // 头部广告
+        put(".banner-ad-box");
+        // 右侧广告
+        put(".slide-outer");
+        // 右侧浮窗
+        put(".csdn-side-toolbar");
+        // 右侧详情
+        put(".persion_article");
+        // 顶部轮播
+        put(".carousel");
+        clean(10);
+        common(5, 10);
+        loop(1);
+    } else if ((blog.test(currentURL) && blockURL === 4) || blog2.test(currentURL)) {
+        l("正在优化个人博客主页体验...");
+        // 常规
+        
+        clean(10);
+    } else if (article.test(currentURL)) {
+        l("正在优化阅读体验...");
+        // 常规
+        // 快来写博客吧
+        put(".blog_tip_box");
+        // 推荐关注用户
+        put(".blog-expert-recommend-box");
+        // 右下角VIP
+        put(".meau-gotop-box");
+        // 广告
+        put(".mediav_ad");
+        put(".pulllog-box");
+        put(".recommend-ad-box");
+        put(".box-shadow");
+        put(".type_hot_word");
+        put(".fourth_column");
+        // 高分辨率时右侧文章推荐
+        put(".right-item");
+        // 广告
+        put("#asideFooter");
+        put("#ad-div");
+        put("#479");
+        put("#480");
+        // 打赏
+        put(".postTime");
+        // 课程推荐
+        put(".t0");
+        // 分享海报
+        put(".shareSuggest");
+        // 底部主题
+        put(".template-box");
+        // 评论区广告
+        put("div#dmp_ad_58");
+        // 打赏
+        put(".reward-user-box");
+        // 右侧打赏按钮
+        put(".to-reward");
+        clean(10);
+        // 填充
+        common(4, 5);
+        // 评论
+        common(1, 30);
+        // 其它
+        common(2, 20);
+        // 循环线程开始
+        loop(2);
+    } else if (bbs.test(currentURL)) {
+        l("正在优化论坛体验...");
+        // 常规
+        // 评论嵌入小广告
+        put(".post_recommend");
+        // 底部推荐
+        put("#post_feed_wrap");
+        // 底部相关文章里面的广告
+        put(".bbs_feed_ad_box");
+        put(".recommend-ad-box");
+        // 底部相关文字里面的热词提示
+        put(".type_hot_word");
+        // 底部蓝色flex属性的广告栏+登录注册框
+        put(".pulllog-box");
+        // 猜你喜欢
+        put(".personalized-recommend-box");
+        // 发帖减半提示
+        put(".totast-box");
+        // 顶部广告
+        put(".recommend-right");
+        clean(10);
+        // 展开
+        common(3, 50);
+    } else if (download.test(currentURL)) {
+        l("正在优化下载页体验...");
+        // 常规
+        put(".fixed_dl");
+        put("indexSuperise");
+        clean(10);
     } else {
-        remove = false;
+        e("不受支持的页面!");
     }
+    l("超级优化完毕。");
+    l("如果觉得好用，来 https://greasyfork.org/zh-CN/scripts/378351 收藏脚本来支持我吧！");
+    l("开源&&提建议：https://github.com/AdlerED/CSDNGreener");
+    l("我的博客：https://www.stackoverflow.wiki/");
+    l("我的微信：1101635162");
+})();
 
-    //删除推荐内容（自定义）
-    if (remove) {
-        $(".recommend-box").remove();
-    }
+function l(log) {
+    console.log("[CSDNGreener] " + log);
+}
 
-    //推荐内容开关
-    $(".blog-content-box").after("<div class='blog-content-box' id='switch'></div>");
-    $(".comment-edit-box").after("<center><font size='1px'>首次点击“显示推荐内容”可能失效，需刷新后再次点击<br><a href='https://greasyfork.org/zh-CN/scripts/378351-%E6%8C%81%E7%BB%AD%E6%9B%B4%E6%96%B0-csdn%E9%A1%B5%E9%9D%A2%E6%B5%AE%E7%AA%97%E5%B9%BF%E5%91%8A%E5%AE%8C%E5%85%A8%E8%BF%87%E6%BB%A4%E5%87%80%E5%8C%96-%E5%87%80%E5%8C%96%E5%A4%8D%E5%88%B6%E5%86%85%E5%AE%B9-%E8%87%AA%E5%8A%A8%E5%B1%95%E5%BC%80-%E8%AE%A9%E4%BD%A0%E4%B8%93%E6%B3%A8%E4%BA%8E%E6%96%87%E7%AB%A0-%E4%B8%8D%E5%BD%B1%E5%93%8D%E5%8A%9F%E8%83%BD%E4%BD%BF%E7%94%A8'>点此收藏脚本，支持作者！</a> CSDN Greener V" + version + "</font></center><br>");
-    if (remove) {
-        $("#switch").append("<button class='hide-recommend-button'>显示推荐内容</button>");
-    } else {
-        $("#switch").append("<button class='hide-recommend-button'>隐藏推荐内容</button>");
-    }
+function e(error) {
+    console.error("[CSDNGreener] " + error);
+}
+
+function clear() {
+    list = [];
+}
+
+function put(tag) {
+    list.push(tag);
+}
+
+function clean(times) {
+    var loop = setInterval(function () {
+        --times;
+        if (times <= 0) {
+            clearInterval(loop);
+        }
+        for (var j = 0; j < list.length; j++) {
+            $(list[j]).remove();
+        }
+    }, 100);
+}
+
+function loop(num) {
+    setInterval(function () {
+        if (num === 1) {
+            // 主页中间的广告
+            $(".J_adv").remove();
+            // 主页有新的内容横条
+            $(".feed-fix-box").remove();
+            // 主页广告iframe
+            if (currentURL == "https://www.csdn.net/") {
+                $("iframe").remove();
+            }
+        } else if (num === 2) {
+            // 评论查看更多展开监听
+            $("div.comment-list-box").css("max-height", "none");
+        }
+    }, 500);
+}
+
+function common(num, times) {
+    var loop = setInterval(function () {
+        --times;
+        if (times <= 0) {
+            clearInterval(loop);
+        }
+        if (num === 1) {
+            // 查看更多
+            $(".btn-readmore").removeClass("no-login");
+            $(".btn-readmore").addClass("fans-read-more");
+            $(".btn-readmore").removeAttr("href");
+            $(".btn-readmore").removeAttr("target");
+            $(".btn-readmore").removeAttr("rel");
+            $(".btn-readmore").click();
+            // 已登录用户展开评论
+            document.getElementById("btnMoreComment").click();
+            // 删除查看更多按钮
+            $("#btnMoreComment").parent("div.opt-box").remove();
+            // 展开内容
+            $("div.comment-list-box").css("max-height", "none");
+            // 改回背景颜色
+            $(".login-mark").remove();
+            // 删除登录框
+            $(".login-box").remove();
+        } else if (num === 2) {
+            // 挡住评论的“出头推荐”
+            if ($(".recommend-box").length > 1) {
+                $(".recommend-box")[0].remove();
+            }
+            // 去外链
+            $("#content_views").off();
+            // 去除推广广告
+            $("li[data-type='ad']").remove();
+            // 去除剪贴板劫持
+            try {
+                csdn.copyright.init("", "", "");
+            } catch (err) {
+            }
+        } else if (num == 3) {
+            //论坛自动展开
+            $(".js_show_topic").click();
+        } else if (num == 4) {
+            // 左侧栏填充屏幕
+            $("aside").remove();
+            $("main").css("width", "95%");
+            // 右侧栏靠右
+            $(".tool-box").css("right", "0px");
+        } else if (num == 5) {
+            // 改回背景颜色
+            $(".login-mark").remove();
+            // 删除登录框
+            $(".login-box").remove();
+        } else if (num == 6) {
+            //推荐内容开关cookie
+            var removeCookie = $.cookie("remove");
+            var remove;
+            if (removeCookie == undefined) {
+                $.cookie('remove', true, {
+                    path: '/'
+                });
+                remove = true;
+            }
+            if (removeCookie == "true") {
+                remove = true;
+            } else {
+                remove = false;
+            }
+            //删除推荐内容（自定义）
+            if (remove) {
+                $(".recommend-box").hide();
+            }
+            //推荐内容开关
+            $(".blog-content-box").after("<div class='blog-content-box' id='switch'></div>");
+            $(".comment-edit-box").after("<center><font size='1px'><a href='https://greasyfork.org/zh-CN/scripts/378351'>CSDN Greener V" + version + "</a><br><a href='https://github.com/AdlerED'>By GitHub :: AdlerED</a></font></center><br>");
+            if (remove) {
+                $("#switch").append("<button class='hide-recommend-button'>显示推荐内容</button>");
+            } else {
+                $("#switch").append("<button class='hide-recommend-button'>隐藏推荐内容</button>");
+            }
+            renderHideButton();
+            //开关监听
+            $(".hide-recommend-button").click(function () {
+                if ($.cookie('remove') == "true") {
+                    $.cookie('remove', false, {
+                        path: '/'
+                    });
+                    $(".recommend-box").show();
+                    $(".hide-recommend-button").html("隐藏推荐内容");
+                } else {
+                    $.cookie('remove', true, {
+                        path: '/'
+                    });
+                    $(".recommend-box").hide();
+                    $(".hide-recommend-button").html("显示推荐内容");
+                }
+                renderHideButton();
+            });
+        }
+    }, 100);
+}
+
+function renderHideButton() {
     $(".hide-recommend-button").css({
         "width": "270px",
         "height": "40px",
         "border-width": "0px",
         "border-radius": "3px",
-        "background": "#A9A9A9",
+        "background": "#f3e3ff",
         "cursor": "pointer",
         "outline": "none",
         "font-family": "Microsoft YaHei",
-        "color": "white",
+        "color": "rgba(15,15,15,0.6)",
         "font-size": "17px",
         "display": "block",
-        "margin": "0 auto",
+        "margin": "0 auto"
     });
     $(".hide-recommend-button").hover(
-
         function () {
-            $(".hide-recommend-button").css("background", "#D3D3D3");
+            $(".hide-recommend-button").css("background", "#e0d1eb");
         }, function () {
-            $(".hide-recommend-button").css("background", "#A9A9A9");
-        });
-
-    //开关监听
-    $(".hide-recommend-button").click(function () {
-        if ($.cookie('remove') == "true") {
-            $.cookie('remove', false, {
-                path: '/'
-            });
-        } else {
-            $.cookie('remove', true, {
-                path: '/'
-            });
+            $(".hide-recommend-button").css("background", "#f3e3ff");
         }
-        location.reload();
-    });
-
-    var starting = setInterval(function () {
-        count++;
-        if (count > 50) {
-            console.log("净化已完成! 请享受绿色的CSDN~");
-            $("#green").html("已净化");
-            clearInterval(starting);
-        } else {
-            if (count >= 5 && count <= 40) {
-                //展开所有内容，包括评论
-                try {
-                    //已登录用户展开评论
-                    document.getElementById("btnMoreComment").click();
-                    //*** 非登录情况下评论展开，请勿借鉴 ***
-                    //删除查看更多按钮
-                    $("#btnMoreComment").parent("div.opt-box").remove();
-                    //展开内容
-                    $("div.comment-list-box").css("max-height", "none");
-                    //**关闭登录提示框**
-                    //改回背景颜色
-                    $(".login-mark").remove();
-                    //删除登录框
-                    $(".login-box").remove();
-                } catch (err) {}
-
-                //查看更多
-                try {
-                    $(".btn-readmore").removeClass("no-login");
-                    $(".btn-readmore").addClass("fans-read-more");
-                    $(".btn-readmore").removeAttr("href");
-                    $(".btn-readmore").removeAttr("target");
-                    $(".btn-readmore").removeAttr("rel");
-                    $(".btn-readmore").click();
-                } catch (err) {}
-                //论坛自动展开
-                $(".js_show_topic").click();
-            }
-            //进度条
-            console.log("正在干掉遗漏的浮窗(不会影响性能, 请无视该消息)......");
-            killAll();
-        }
-    }, 100);
-
-    //由于部分交互非即时打开, 所以一直循环
-    setInterval(function () {
-        //主页中间的广告
-        $(".J_adv").remove();
-        //主页有新的内容横条
-        $(".feed-fix-box").remove();
-        //主页广告iframe
-        if (currentURL == "https://www.csdn.net/") {
-            $("iframe").remove();
-        }
-        //评论查看更多展开监听
-        $("div.comment-list-box").css("max-height", "none");
-    }, 500);
-})();
-
-function killAll() {
-    //核心代码
-    //CLASSES
-    var classList = new Array(
-        // ****** 主页 *****
-        //头部广告
-        "banner-ad-box",
-        //右侧广告
-        "slide-outer",
-        //顶栏无用选项
-        //APP
-        "app-btn",
-        //VIP
-        "vip-caise",
-        //右侧浮窗(客服、客户端二维码)
-        "csdn-side-toolbar",
-        //右侧详情
-        "persion_article",
-        //顶部轮播
-        "carousel",
-        // ****** 文章页 ******
-        //快来写博客吧
-        "blog_tip_box",
-        //推荐关注用户
-        "blog-expert-recommend-box",
-        //右侧工具栏（注释原因：收藏消失）
-        //"tool-box",
-        //右下角VIP
-        "meau-gotop-box",
-        //广告
-        "mediav_ad", "pulllog-box", "recommend-ad-box", "box-shadow", "type_hot_word", "fourth_column",
-        //高分辨率时右侧文章推荐
-        "right-item",
-        // ****** 下载 ******
-        "fixed_dl", "indexSuperise",
-        // ****** 论坛 ******
-        //推荐广告
-        //底部相关文章里面的广告
-        "bbs_feed_ad_box", 'recommend-ad-box',
-        //底部相关文字里面的热词提示
-        'type_hot_word',
-        //底部蓝色flex属性的广告栏+登录注册框
-        'pulllog-box',
-        //猜你喜欢（我不喜欢）
-        "personalized-recommend-box",
-        //发帖减半提示
-        "totast-box",
-        //顶部广告
-        "recommend-right",
-        //打赏
-        "postTime",
-        //课程推荐广告
-        "t0",
-        //分享海报提示
-        "shareSuggest",
-        //底部主题信息
-        "template-box", );
-
-    //IDS
-    var idList = new Array(
-        // ****** 文章页 ******
-        //广告
-        "asideFooter", "ad-div", "479", "480",
-        // ****** 论坛 ******
-        //底部推荐
-        "post_feed_wrap", );
-
-    for (var i = 0; i < classList.length; i++) {
-        var tempName = classList[i];
-        $("." + tempName).remove();
-    }
-
-    for (var j = 0; j < idList.length; j++) {
-        var tempID = idList[j];
-        $("#" + tempID).remove();
-    }
+    );
 }
