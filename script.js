6f90f0e3a6b4c29fa16b54df1ba29e57ab988d80
Adler Chan <AdlerED@outlook.com>
2020-03-12 10:51:01 +0800

    :art: 2.1.6 宽度自适应（感谢LeonG7 #20 ）！修复剪贴板净化无效的问题。

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         (持续更新)CSDN页面浮窗广告完全过滤净化(净化复制内容|自动展开|让你专注于文章|不影响功能使用)
 // @namespace    https://github.com/AdlerED
-// @version      2.1.5
+// @version      2.1.6
 // @description  ⚡️拥有数项独家功能的最强脚本，不服比一比⚡️|✔️CSDN体验秒杀AdBlock|✔️超级预优化|✔️独家超级免会员|✔️独家原创文章免登录展开|✔️独家推荐内容自由开关|✔️独家免登录复制|✔️独家防外链重定向|✔️独家论坛未登录自动展开文章、评论|✔️全面净化|✔️沉浸阅读|✔️净化剪贴板|✔️作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js
+// @note         20-03-12 2.1.6 宽度自适应（感谢来自GitHub的朋友LeonG7的建议）！修复剪贴板净化无效的问题。
 // @note         20-03-04 2.1.5 适配AdGuard
 // @note         20-02-27 2.1.4 优化免登录复制
 // @note         20-02-25 2.1.3 免登录复制更新，现已可用
@@ -52,7 +53,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "2.1.5";
+var version = "2.1.6";
 var currentURL = window.location.href;
 var list;
 
@@ -270,6 +271,10 @@ function loop(num) {
         } else if (num === 2) {
             // 评论查看更多展开监听
             $("div.comment-list-box").css("max-height", "none");
+            // 文章宽度自适应
+            if (window.innerWidth < 1100) {
+                $("article").width(window.innerWidth - 150);
+            }
         }
     }, 500);
 }
@@ -322,6 +327,7 @@ function common(num, times) {
                 });
             } catch (err) {
             }
+            csdn.copyright.init("", "", "");
             // 页头广告
             document.getElementsByClassName("column-advert-box")[0].style.display="none";
         } else if (num == 3) {
