8a25a42c2a5361feb87394b47191af6ce5afabf4
Adler Chan <AdlerED@outlook.com>
2020-03-13 10:29:23 +0800

    :art: 2.1.8 窄屏适配加强

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         (持续更新)CSDN页面浮窗广告完全过滤净化(净化复制内容|自动展开|让你专注于文章|不影响功能使用)
 // @namespace    https://github.com/AdlerED
-// @version      2.1.7
+// @version      2.1.8
 // @description  ⚡️拥有数项独家功能的最强脚本，不服比一比⚡️|✔️CSDN体验秒杀AdBlock|✔️分辨率自适配，分屏不用滚动|✔️超级预优化|✔️独家超级免会员|✔️独家原创文章免登录展开|✔️独家推荐内容自由开关|✔️独家免登录复制|✔️独家防外链重定向|✔️独家论坛未登录自动展开文章、评论|✔️全面净化|✔️沉浸阅读|✔️净化剪贴板|✔️作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js
+// @note         20-03-13 2.1.8 窄屏适配加强
 // @note         20-03-13 2.1.7 更新简介
 // @note         20-03-12 2.1.6 宽度自适应（感谢来自GitHub的朋友LeonG7的建议）！修复剪贴板净化无效的问题。
 // @note         20-03-04 2.1.5 适配AdGuard
@@ -54,7 +55,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "2.1.7";
+var version = "2.1.8";
 var currentURL = window.location.href;
 var list;
 
@@ -251,6 +252,7 @@ function clean(times) {
 }
 
 function loop(num) {
+    var did = false;
     setInterval(function () {
         if (num === 1) {
             // 主页中间的广告
@@ -275,6 +277,12 @@ function loop(num) {
             // 文章宽度自适应
             if (window.innerWidth < 1100) {
                 $("article").width(window.innerWidth - 150);
+                did = true;
+            } else {
+                if (did === true) {
+                    $("article").removeAttr("style");
+                    did = false;
+                }
             }
         }
     }, 500);
@@ -317,8 +325,8 @@ function common(num, times) {
             $(".hljs-button").removeClass("signin");
             $(".hljs-button").addClass("{2}");
             $(".hljs-button").attr("data-title", "免登录复制");
-            $("code").attr("onclick", "mdcp.copyCode(event)");
             // 去除剪贴板劫持
+            $("code").attr("onclick", "mdcp.copyCode(event)");
             try {
                 // 复制时保留原文格式，参考 https://greasyfork.org/en/scripts/390502-csdnremovecopyright/code
                 Object.defineProperty(window, "articleType", {
