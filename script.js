0c355d5c24d7c2574cc842fbbbbefd78f7a9c0d1
Adler Chan <404x@139.com>
2020-06-14 15:06:30 +0800

    :tada: 3.1.3 绿化设定优化

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🔥持续更新🔥 CSDN广告完全过滤、人性化脚本优化：🆕 不用再登录了！让你体验令人惊喜的崭新CSDN。
 // @namespace    https://github.com/adlered
-// @version      3.1.2
+// @version      3.1.3
 // @description  ⚡️拥有数项独家功能的最强CSDN脚本，不服比一比⚡️|🕶无需登录CSDN，获得比会员更佳的体验|🖥分辨率自适配，分屏不用滚动|💾超级预优化|🔖独家超级免会员|🏷独家原创文章免登录展开|🔌独家推荐内容自由开关|📠独家免登录复制|🔗独家防外链重定向|📝独家论坛未登录自动展开文章、评论|🌵全面净化|📈沉浸阅读|🧴净化剪贴板|📕作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
@@ -12,6 +12,7 @@
 // @supportURL   https://github.com/adlered/CSDNGreener/issues/new?assignees=adlered&labels=help+wanted&template=ISSUE_TEMPLATE.md&title=
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @grant        GM_addStyle
+// @note         20-06-14 3.1.3 绿化设定优化
 // @note         20-06-14 3.1.2 ISSUE模板调整Support URL
 // @note         20-06-14 3.1.1 增加搜博主文章模块
 // @note         20-06-13 3.1.0 修复设置过期的问题
@@ -97,7 +98,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.1.2";
+var version = "3.1.3";
 var currentURL = window.location.href;
 var list;
 
@@ -105,9 +106,11 @@ var list;
 // 进度条
 $('head').append("<style>#nprogress{pointer-events:none}#nprogress .bar{background:#f44444;position:fixed;z-index:1031;top:0;left:0;width:100%;height:2px}#nprogress .peg{display:block;position:absolute;right:0;width:100px;height:100%;box-shadow:0 0 10px #f44444,0 0 5px #f44444;opacity:1;-webkit-transform:rotate(3deg) translate(0,-4px);-ms-transform:rotate(3deg) translate(0,-4px);transform:rotate(3deg) translate(0,-4px)}#nprogress .spinner{display:block;position:fixed;z-index:1031;top:15px;right:15px}#nprogress .spinner-icon{width:18px;height:18px;box-sizing:border-box;border:solid 2px transparent;border-top-color:#f44444;border-left-color:#f44444;border-radius:50%;-webkit-animation:nprogress-spinner .4s linear infinite;animation:nprogress-spinner .4s linear infinite}.nprogress-custom-parent{overflow:hidden;position:relative}.nprogress-custom-parent #nprogress .bar,.nprogress-custom-parent #nprogress .spinner{position:absolute}@-webkit-keyframes nprogress-spinner{0%{-webkit-transform:rotate(0)}100%{-webkit-transform:rotate(360deg)}}@keyframes nprogress-spinner{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}</style>");
 // 弹出窗口
-$('head').append("<style>.black_overlay{top:0%;left:0%;width:100%;height:100%;background-color:#000;z-index:1001;-moz-opacity:0.8;opacity:.20;filter:alpha(opacity=88)}.black_overlay,.white_content{display:none;position:absolute}.white_content{top:25%;left:25%;width:40%;height:300px;padding:20px;border:0px;background-color:#fff;z-index:1002;overflow:auto}</style>");
+$('head').append("<style>.black_overlay{top:0%;left:0%;width:100%;height:100%;background-color:#000;z-index:1001;-moz-opacity:0.8;opacity:.20;filter:alpha(opacity=88)}.black_overlay,.white_content{display:none;position:absolute}.white_content{top:25%;left:25%;width:40%;height:320px;padding:20px;border:0px;background-color:#fff;z-index:1002;overflow:auto}</style>");
 // 提示条
 $('head').append("<style>.tripscon{padding:10px}</style>");
+// 保存按钮
+$('head').append("<style>#save{background-color:#4caf50;border:none;color:#fff;padding:6px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:14px;margin:15px 0px;cursor:pointer}</style>");
 
 (function () {
     'use strict';
@@ -490,11 +493,14 @@ function common(num, times) {
             configHTML += '<input type="checkbox" id="toggle-whitetheme-button"> <span class="modeLabel">强制白色主题（关闭后，可以安装 Dark Reader 浏览器扩展适配 CSDN 黑暗模式）</span>';
             configHTML += '<br>';
             configHTML += '<input type="checkbox" id="toggle-searchblog-button"> <span class="modeLabel">搜博主文章模块</span>';
+            configHTML += '<br>';
+            configHTML += '<button id="save" onclick="location.reload()">保存设定</button>';
 
             // 绿化器设定
             $("body").prepend('<div id="light" class="white_content">' + configHTML + '<a href="https://github.com/adlered/CSDNGreener" target="_blank" style="position: absolute; bottom: 10px; left: 10px;">⭐ 开发动力, 求个Star</a><a href="javascript:void(0)" style="position: absolute; bottom: 10px; right: 10px;" onclick=\'document.getElementById("light").style.display="none",document.getElementById("fade").style.display="none"\'>关闭设置窗口 ✖️</a></div><div id="fade" class="black_overlay"></div> ');
             // 绿化设定
-            $("#nav-left-menu").prepend('<li><a id="greenerSettings" href="javascript:void(0)" style="font-weight: bold;" onclick="$(window).scrollTop(0);document.getElementById(\'light\').style.display=\'block\';document.getElementById(\'fade\').style.display=\'block\';">🌵 绿化设定</a></li>');
+            let htmlOf0 = '<li><a id="greenerSettings" href="javascript:void(0)" style="" onclick="$(window).scrollTop(0);document.getElementById(\'light\').style.display=\'block\';document.getElementById(\'fade\').style.display=\'block\';">绿化设定</a></li>';
+            $(".sub-menu-box").eq(0).before(htmlOf0);
 
             /** 配置控制 **/
             let config = new Config();
@@ -551,11 +557,11 @@ function common(num, times) {
                                 function() {location.reload();});
 
             // 提示
-            let tipsCookie = config.get("tips", true);
+            let tipsCookie = config.get("tips" + version, true);
             if (tipsCookie) {
                 showTips();
             }
-            config.set("tips", false);
+            config.set("tips" + version, false);
 
             // 显示作者名片
             let authorCardCookie = config.get("authorCard", false);
@@ -687,13 +693,13 @@ class Config {
             if (status === "true" || status) {
                 console.log("Key set: " + listenKey + " :: " + false);
                 new Config().set(listenKey, false);
-                falseAction();
-                $(element).prop("checked", false);
+                // falseAction();
+                // $(element).prop("checked", false);
             } else {
                 console.log("Key set: " + listenKey + " :: " + true);
                 new Config().set(listenKey, true);
-                trueAction();
-                $(element).prop("checked", true);
+                // trueAction();
+                // $(element).prop("checked", true);
             }
         });
     }
@@ -701,7 +707,7 @@ class Config {
 
 function showTips() {
 	var config = {
-		content: "欢迎使用 CSDNGreener，点击这里进入绿化设置！<br><a href='javascript:$(\".trips\").remove();'>朕知道了，已阅</a>",
+		content: "欢迎使用 CSDNGreener V" + version + "，绿化设定按钮迁移到这里了哦~<br><a href='javascript:$(\".trips\").remove();'>好的，该版本不再提示</a>",
 		type: "html",
 		alignTo: ["bottom", "left"],
 		trigger: "show",
