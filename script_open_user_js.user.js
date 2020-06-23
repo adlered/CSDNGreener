9942fb66f25a5bc8b84ae129be884cc119cfe8b6
Adler Chan <404x@139.com>
2020-06-23 11:14:17 +0800

    :art: 3.2.7 右侧栏滚动条显示方式优化

--- script_open_user_js.user.js
+++ script_open_user_js.user.js
@@ -8,7 +8,7 @@
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @name         最强的老牌脚本CSDNGreener：CSDN广告完全过滤、人性化脚本优化
 // @namespace    https://github.com/adlered
-// @version      3.2.6
+// @version      3.2.7
 // @description  拥有数项独家功能的最强CSDN脚本，不服比一比|无需登录CSDN，获得比会员更佳的体验|模块化卡片，显示什么你决定|分辨率自适配，分屏不用滚动|超级预优化|独家原创文章免登录展开|独家推荐内容自由开关|独家免登录复制|独家防外链重定向|独家论坛未登录自动展开文章、评论|全面净化|沉浸阅读|净化剪贴板
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
@@ -16,6 +16,7 @@
 // @require      https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.js
 // @require      https://cdn.jsdelivr.net/gh/adlered/bolo-solo/src/main/webapp/js/lib/jquery/jquery.showtips.js
 // @grant        GM_addStyle
+// @note         20-06-23 3.2.7 右侧栏滚动条显示方式优化
 // @note         20-06-23 3.2.6 文章居中模式可选
 // @note         20-06-22 3.2.5 右侧没有可显示内容时，文章居中
 // @note         20-06-22 3.2.4 文章居中
@@ -114,7 +115,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.2.6";
+var version = "3.2.7";
 var currentURL = window.location.href;
 var list;
 var windowTop = 0;
@@ -922,13 +923,14 @@ function common(num, times) {
             setTimeout(function () {
                 let rightSideHeight = 0;
                 let pageHeight = $(window).height();
-                rightSideHeight += getHeight("#asideProfile");
-                rightSideHeight += getHeight("#asideSearchArticle");
-                rightSideHeight += getHeight("#asideNewArticle");
-                rightSideHeight += getHeight("#asideHotArticle");
-                rightSideHeight += getHeight("#asideNewComments");
-                rightSideHeight += getHeight("#asideCategory");
-                rightSideHeight += getHeight("#asideArchive");
+                rightSideHeight += getHeight($('.align-items-stretch.group_item').parent());
+                rightSideHeight += getHeight($("#asideProfile"));
+                rightSideHeight += getHeight($("#asideSearchArticle"));
+                rightSideHeight += getHeight($("#asideNewArticle"));
+                rightSideHeight += getHeight($("#asideHotArticle"));
+                rightSideHeight += getHeight($("#asideNewComments"));
+                rightSideHeight += getHeight($("#asideCategory"));
+                rightSideHeight += getHeight($("#asideArchive"));
                 l("Right side total height: " + rightSideHeight);
                 l("Page height: " + pageHeight);
                 if (rightSideHeight > pageHeight) {
@@ -1029,7 +1031,7 @@ function showTips() {
 }
 
 function getHeight(element) {
-    let outerHeight = $(element).outerHeight();
+    let outerHeight = element.outerHeight();
     if (outerHeight === null) {
         return 0;
     }
