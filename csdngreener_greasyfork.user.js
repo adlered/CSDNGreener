6e18e68cdfad215beec45ffea38968464a32516a
Adler Chan <404x@139.com>
2020-12-15 14:56:25 +0800

    :art: 3.4.4 修复了某些子页显示不正常的问题

--- csdngreener_greasyfork.user.js
+++ csdngreener_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🔥持续更新🔥 CSDN广告完全过滤、人性化脚本优化：🆕 不用再登录了！让你体验令人惊喜的崭新CSDN。
 // @namespace    https://github.com/adlered
-// @version      3.4.3
+// @version      3.4.4
 // @description  ⚡️拥有数项独家功能的最强CSDN脚本，不服比一比⚡️|🕶无需登录CSDN，获得比会员更佳的体验|🖥分辨率自适配，分屏不用滚动|💾超级预优化|🔖独家超级免会员|🏷独家原创文章免登录展开|🔌独家推荐内容自由开关|📠独家免登录复制|🔗独家防外链重定向|📝独家论坛未登录自动展开文章、评论|🌵全面净化|📈沉浸阅读|🧴净化剪贴板|📕作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
@@ -12,6 +12,7 @@
 // @supportURL   https://github.com/adlered/CSDNGreener/issues/new?assignees=adlered&labels=help+wanted&template=ISSUE_TEMPLATE.md&title=
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @grant        GM_addStyle
+// @note         20-12-15 3.4.4 修复了某些子页显示不正常的问题
 // @note         20-10-23 3.4.3 适应新版CSDN，去除主页和登录页广告，以及登录提示，并移除底部信息
 // @note         20-10-20 3.4.2 删除右侧广告
 // @note         20-09-26 3.4.1 修改排版设定，修复登录框弹出的问题
@@ -120,10 +121,12 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.4.3";
+var version = "3.4.4";
 var currentURL = window.location.href;
 var list;
 var windowTop = 0;
+var startTimeMilli = Date.now();
+var stopTimeMilli = 0;
 
 // 自定义 CSS
 // 进度条
@@ -530,9 +533,11 @@ var set_svg = '<svg t="1592982970375" class="icon" viewBox="0 0 1024 1024" versi
             put(".mediav_ad");
             put(".pulllog-box");
             put(".recommend-ad-box");
-            put(".box-shadow");
+            //put(".box-shadow"); 某些页面异常，例如cloud.csdn.net
             put(".type_hot_word");
             put(".fourth_column");
+            // cloud.csdn.net 头部广告
+            put("#kp_box_118");
             // 广告
             put("#asideFooter");
             put("#ad-div");
@@ -607,7 +612,8 @@ var set_svg = '<svg t="1592982970375" class="icon" viewBox="0 0 1024 1024" versi
         setTimeout(function() {
             NProgress.done();
         }, 0);
-        l("优化完毕!");
+        stopTimeMilli = Date.now();
+        l("优化完毕! 耗时 " + (stopTimeMilli - startTimeMilli) + "ms");
     }, 0);
 })();
 
