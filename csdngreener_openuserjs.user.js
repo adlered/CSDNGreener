68dc6251b2e132589f20603cace81ed1036eb991
Adler Chan <404x@139.com>
2020-12-18 14:35:04 +0000

    :art: 3.4.5 修复绿化设定按钮排版不正确的问题

--- csdngreener_openuserjs.user.js
+++ csdngreener_openuserjs.user.js
@@ -8,7 +8,7 @@
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @name         最强的老牌脚本CSDNGreener：CSDN广告完全过滤、人性化脚本优化
 // @namespace    https://github.com/adlered
-// @version      3.4.4
+// @version      3.4.5
 // @description  拥有数项独家功能的最强CSDN脚本，不服比一比|无需登录CSDN，获得比会员更佳的体验|模块化卡片，显示什么你决定|分辨率自适配，分屏不用滚动|超级预优化|独家原创文章免登录展开|独家推荐内容自由开关|独家免登录复制|独家防外链重定向|独家论坛未登录自动展开文章、评论|全面净化|沉浸阅读|净化剪贴板
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
@@ -17,6 +17,7 @@
 // @require      https://cdn.jsdelivr.net/gh/adlered/bolo-solo/src/main/webapp/js/lib/jquery/jquery.showtips.js
 // @require      https://cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js
 // @grant        GM_addStyle
+// @note         20-12-18 3.4.5 修复绿化设定按钮排版不正确的问题
 // @note         20-12-15 3.4.4 修复了某些子页显示不正常的问题
 // @note         20-10-23 3.4.3 适应新版CSDN，去除主页和登录页广告，以及登录提示，并移除底部信息
 // @note         20-10-20 3.4.2 删除右侧广告
@@ -133,7 +134,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.4.4";
+var version = "3.4.5";
 var currentURL = window.location.href;
 var list;
 var windowTop = 0;
@@ -310,6 +311,8 @@ var set_svg = '<svg t="1592982970375" class="icon" viewBox="0 0 1024 1024" versi
             put(".toolbar-advert");
             // 底部信息
             put(".blog-footer-bottom");
+            // 右侧栏广告
+            put("#6527");
             clean(10);
             setTimeout(function() {
                // 展开评论的所有回复
@@ -765,7 +768,7 @@ function common(num, times) {
             $("body").prepend('<div id="light" class="white_content">' + configHTML);
             // 绿化设定
             let htmlOf0 = '<li><a id="greenerSettings" href="javascript:void(0)" style="" onclick="$(window).scrollTop(0);document.getElementById(\'light\').style.display=\'block\';document.getElementById(\'fade\').style.display=\'block\';">' + set_svg + ' 绿化设定</a></li>';
-            $(".toolbar-subMenu-box").eq(1).before(htmlOf0);
+            $(".toolbar-menus > li").eq(0).before(htmlOf0);
 
             /** 配置控制 **/
             let config = new Config();
