fc32ca1de3ecbb801df714bbfd6bfed702a32eae
陈辉 <404x@139.com>
2021-01-17 13:41:20 +0800

    :art: 3.4.9 删除文章页和论坛广告，暂时停用右侧栏滚动功能（CSDN限制）

--- csdngreener_openuserjs.user.js
+++ csdngreener_openuserjs.user.js
@@ -8,7 +8,7 @@
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @name         最强的老牌脚本CSDNGreener：CSDN广告完全过滤、人性化脚本优化
 // @namespace    https://github.com/adlered
-// @version      3.4.8
+// @version      3.4.9
 // @description  拥有数项独家功能的最强CSDN脚本，不服比一比|无需登录CSDN，获得比会员更佳的体验|模块化卡片，显示什么你决定|分辨率自适配，分屏不用滚动|超级预优化|独家原创文章免登录展开|独家推荐内容自由开关|独家免登录复制|独家防外链重定向|独家论坛未登录自动展开文章、评论|全面净化|沉浸阅读|净化剪贴板
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
@@ -17,6 +17,7 @@
 // @require      https://cdn.jsdelivr.net/gh/adlered/bolo-solo/src/main/webapp/js/lib/jquery/jquery.showtips.js
 // @require      https://cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js
 // @grant        GM_addStyle
+// @note         21-01-17 3.4.9 删除文章页和论坛广告，暂时停用右侧栏滚动功能（CSDN限制）
 // @note         21-01-15 3.4.8 保存按钮优化，修复显示创作中心按钮功能失效的问题
 // @note         21-01-15 3.4.7 改进脚本细节，增加广告屏蔽能力，修复绿化按钮错位的问题
 // @note         20-12-25 3.4.6 主页部分嵌入式广告删除
@@ -137,7 +138,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.4.8";
+var version = "3.4.9";
 var currentURL = window.location.href;
 if (currentURL.indexOf("?") !== -1) {
 currentURL = currentURL.substring(0, currentURL.indexOf("?"));
@@ -323,6 +324,7 @@ var set_svg = '<svg t="1592982970375" class="icon" viewBox="0 0 1024 1024" versi
             put(".blog-footer-bottom");
             // 右侧栏广告
             put("#6527");
+            put("#recommendAdBox");
             clean(10);
             setTimeout(function() {
                // 展开评论的所有回复
@@ -371,6 +373,8 @@ var set_svg = '<svg t="1592982970375" class="icon" viewBox="0 0 1024 1024" versi
             put(".recommend-right");
             // 顶部广告
             put(".ad_top");
+            // 右侧广告
+            put(".ad_1");
             clean(10);
             // 展开
             common(3, 50);
@@ -1139,7 +1143,7 @@ function common(num, times) {
                                function() {location.reload();});
 
             // 右侧滚动条
-            setTimeout(function () {
+            /** setTimeout(function () {
                 let rightSideHeight = 0;
                 let pageHeight = $(window).height();
                 rightSideHeight += getHeight($('.align-items-stretch.group_item').parent());
@@ -1155,7 +1159,7 @@ function common(num, times) {
                 if (rightSideHeight > pageHeight) {
                     $('#recommend-right').css("overflow", "scroll");
                 }
-            }, 1500);
+            }, 1500); */
         } else if (num === 7) {
             $(".me_r")[1].remove();
         } else if (num === 8) {
