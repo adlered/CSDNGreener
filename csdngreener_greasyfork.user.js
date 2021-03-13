d269fe52575d85055d8101d642153894d0baf34a
陈辉 <adlered@foxmail.com>
2021-03-13 21:24:21 +0800

    :art: 3.5.2 去主页广告，去文章页面推荐内容Title

--- csdngreener_greasyfork.user.js
+++ csdngreener_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🔥持续更新🔥 CSDN广告完全过滤、人性化脚本优化：🆕 不用再登录了！让你体验令人惊喜的崭新CSDN。
 // @namespace    https://github.com/adlered
-// @version      3.5.1
+// @version      3.5.2
 // @description  ⚡️拥有数项独家功能的最强CSDN脚本，不服比一比⚡️|🕶无需登录CSDN，获得比会员更佳的体验|🖥分辨率自适配，分屏不用滚动|💾超级预优化|🔖独家超级免会员|🏷独家原创文章免登录展开|🔌独家推荐内容自由开关|📠独家免登录复制|🔗独家防外链重定向|📝独家论坛未登录自动展开文章、评论|🌵全面净化|📈沉浸阅读|🧴净化剪贴板|📕作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
@@ -12,6 +12,7 @@
 // @supportURL   https://github.com/adlered/CSDNGreener/issues/new?assignees=adlered&labels=help+wanted&template=ISSUE_TEMPLATE.md&title=
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @grant        GM_addStyle
+// @note         21-03-13 3.5.2 去主页广告，去文章页面推荐内容Title
 // @note         21-03-01 3.5.1 修改文案
 // @note         21-02-06 3.5.0 修复上传资源界面标签选择消失的问题
 // @note         21-01-17 3.4.9 删除文章页和论坛广告，暂时停用右侧栏滚动功能（CSDN限制）
@@ -128,7 +129,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.5.1";
+var version = "3.5.2";
 var currentURL = window.location.href;
 if (currentURL.indexOf("?") !== -1) {
 currentURL = currentURL.substring(0, currentURL.indexOf("?"));
@@ -352,6 +353,8 @@ var save_svg = '<svg t="1610717500033" class="icon" style="width: 1em; height: 1
             put(".toolbar-advert");
             // 顶栏VIP选项
             $('.toolbar-subMenu-box').find("[href='https://mall.csdn.net/vip']").parent().remove();
+            // CSDN工具广告
+            put("#floor-ad_64");
             clean(10);
             // common(5, 10);
             // 博客及主页优化
@@ -446,6 +449,8 @@ var save_svg = '<svg t="1610717500033" class="icon" style="width: 1em; height: 1
             // 右侧栏广告
             put("#6527");
             put("#recommendAdBox");
+            // 推荐内容Title
+            put(".recommend-tit-mod");
             clean(10);
             setTimeout(function() {
                // 展开评论的所有回复
