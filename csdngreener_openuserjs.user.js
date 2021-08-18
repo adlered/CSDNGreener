4c2a7ad96ef34e62a13c1e9e2ce17841978e242e
陈辉 <adlered@foxmail.com>
2021-08-18 16:08:20 +0800

    :art: 3.5.5 修复无法选择复制的问题

--- csdngreener_openuserjs.user.js
+++ csdngreener_openuserjs.user.js
@@ -8,7 +8,7 @@
 // @contributionURL https://doc.stackoverflow.wiki/web/#/21?page_id=138
 // @name         最强的老牌脚本CSDNGreener：CSDN广告完全过滤、人性化脚本优化
 // @namespace    https://github.com/adlered
-// @version      3.5.4
+// @version      3.5.5
 // @description  拥有数项独家功能的最强CSDN脚本，不服比一比|无需登录CSDN，获得比会员更佳的体验|模块化卡片，显示什么你决定|分辨率自适配，分屏不用滚动|超级预优化|独家原创文章免登录展开|独家推荐内容自由开关|独家免登录复制|独家防外链重定向|独家论坛未登录自动展开文章、评论|全面净化|沉浸阅读|净化剪贴板
 // @connect      www.csdn.net
 // @include      *://*.csdn.net/*
@@ -16,6 +16,7 @@
 // @require      https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.js
 // @require      https://cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js
 // @grant        GM_addStyle
+// @note         21-08-18 3.5.5 修复无法选择复制的问题
 // @note         21-06-17 3.5.4 去除右侧红包悬浮窗
 // @note         21-04-18 3.5.3 增加显示小店的设定
 // @note         21-03-13 3.5.2 去主页广告，去文章页面推荐内容Title
@@ -142,7 +143,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "3.5.4";
+var version = "3.5.5";
 var currentURL = window.location.href;
 if (currentURL.indexOf("?") !== -1) {
 currentURL = currentURL.substring(0, currentURL.indexOf("?"));
@@ -477,6 +478,10 @@ var save_svg = '<svg t="1610717500033" class="icon" style="width: 1em; height: 1
             $(".toolbar-menus > li > a[href='https://edu.csdn.net/']").parent().remove();
             $(".toolbar-menus > li > a[href='https://live.csdn.net/?utm_source=csdn_toolbar']").parent().remove();
             $(".toolbar-menus > li > a[href='https://book.csdn.net/']").parent().remove();
+            // 修复无法选择复制
+            $("code").css("user-select","auto");
+            $("#content_views").css("user-select","auto");
+            $("pre").css("user-select","auto");7
             // 绿化设置
             common(6, 1);
             // 屏幕适配
