581ccac135a25d3b009213eb2f2dad5e7ec84a0c
Ujhhgtg <feyxiexzf@gmail.com>
2022-06-12 13:54:52 +0800

    Update csdngreener_greasyfork.user.js

--- csdngreener_greasyfork.user.js
+++ csdngreener_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         🔥持续更新🔥 CSDN广告完全过滤、人性化脚本优化：🆕 不用再登录了！让你体验令人惊喜的崭新CSDN。
+// @name         CSDN优化脚本 - 不含使用情况统计的版本
 // @namespace    https://github.com/adlered
-// @version      4.1.3
+// @version      4.1.4
 // @license      MIT
 // @description  ⚡️全新4.0版本！拥有数项独家功能的最强CSDN脚本，不服比一比⚡️|🕶无需登录CSDN，获得比会员更佳的体验|🖥自定义背景图，分辨率自适配，分屏不用滚动|💾超级预优化|🔖独家超级免会员|🏷独家原创文章免登录展开|🔌独家推荐内容自由开关|📠独家免登录复制|🔗独家防外链重定向|📝独家论坛未登录自动展开文章、评论|🌵全面净化|📈沉浸阅读|🧴净化剪贴板|📕作者信息文章顶部展示
 // @author       Adler & Ujhhgtg
@@ -15,6 +15,7 @@
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @note         22-06-12 4.1.4 更新脚本名称
 // @note         22-06-09 4.1.3 在代码内添加license；同时由于PR不被接受，在代码内添加作者
 // @note         22-06-07 4.1.2 尝试去除用户跟踪功能
 // @note         22-05-30 4.1.1 功能修复，广告屏蔽
@@ -151,7 +152,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "4.1.3";
+var version = "4.1.4";
 var currentURL = window.location.href;
 if (currentURL.indexOf("?") !== -1) {
     currentURL = currentURL.substring(0, currentURL.indexOf("?"));
