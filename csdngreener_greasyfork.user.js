d1058e42bb25750eb1ed804925d61e98665d16a5
陈辉 <adlered@foxmail.com>
2023-02-03 16:49:38 +0800

    :art: 4.1.2 修复了无法登录的问题（评论不登录无法加载暂无解决方案，我们在持续努力中）

--- csdngreener_greasyfork.user.js
+++ csdngreener_greasyfork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🔥持续更新🔥 CSDN广告完全过滤、人性化脚本优化：🆕 不用再登录了！让你体验令人惊喜的崭新CSDN。
 // @namespace    https://github.com/adlered
-// @version      4.1.1
+// @version      4.1.2
 // @description  ⚡️全新4.0版本！拥有数项独家功能的最强CSDN脚本，不服比一比⚡️|🕶无需登录CSDN，获得比会员更佳的体验|🖥自定义背景图，分辨率自适配，分屏不用滚动|💾超级预优化|🔖独家超级免会员|🏷独家原创文章免登录展开|🔌独家推荐内容自由开关|📠独家免登录复制|🔗独家防外链重定向|📝独家论坛未登录自动展开文章、评论|🌵全面净化|📈沉浸阅读|🧴净化剪贴板|📕作者信息文章顶部展示
 // @author       Adler
 // @connect      www.csdn.net
@@ -15,6 +15,7 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @antifeature  tracking ============================================>>> 说明：我们仅会在CSDN页面收集您使用CSDNGreener的情况，帮助我们了解CSDNGreener的用户数量。这个操作仅会收集您的IP地址信息，不包含您鼠标、键盘点击在内的所有操作，没有任何安全风险，不会产生性能损耗。为了保护您的知情权以及使用体验，特告知于您。代码开源可审计，CSDNGreener老牌脚本，有口皆碑。请您放心安装。 <<<============================================
+// @note         23-02-03 4.1.2 修复了无法登录的问题（评论不登录无法加载暂无解决方案，我们在持续努力中）
 // @note         22-05-30 4.1.1 功能修复，广告屏蔽
 // @note         22-01-18 4.1.0 代码折叠适配
 // @note         22-01-05 4.0.9 更新广告
@@ -149,7 +150,7 @@
 // @note         19-03-01 1.0.1 修复了排版问题, 优化了代码结构
 // @note         19-02-26 1.0.0 初版发布
 // ==/UserScript==
-var version = "4.1.1";
+var version = "4.1.2";
 var currentURL = window.location.href;
 if (currentURL.indexOf("?") !== -1) {
     currentURL = currentURL.substring(0, currentURL.indexOf("?"));
@@ -926,7 +927,7 @@ function loop(num) {
             $(".toolbar-advert").remove();
         } else if (num == 3) {
             // 循环删除登录提示框
-            $(".passport-login-container").remove();
+            //$(".passport-login-container").remove();
             // 红包雨
             $("#csdn-redpack").remove();
         }
